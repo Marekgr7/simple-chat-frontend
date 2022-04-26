8878f2c8b5702c35eb9975a6abcc8a12fed23ceb
Marekgr7 <Marekgr7@gmail.com>
2022-04-26 12:34:49 +0200

    test if chatboxes are rendered

--- src/MessengerPage/MessengerPage.test.js
+++ src/MessengerPage/MessengerPage.test.js
@@ -1,25 +1,48 @@
 import React from "react";
 import { screen, waitFor } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 import user from "@testing-library/user-event";
 import { renderWithContext } from "../testUtils";
 import messages from "./MessengerPage.messages";
-
 import MessengerPage from "./MessengerPage";
+import { setOnlineUsers } from "../MessengerPage/messengerSlice";
+import { DUMMY_ONLINE_USERS } from "../MessengerPage/messengerSlice.dummy";
 
-const defaultName = "Stranger";
+const defaultNick = "Stranger";
 
-describe("rendering chatboxes", () => {
+describe("rendering MessengerPage", () => {
   it("renders initial MessengerPage component", () => {
+    window.HTMLElement.prototype.scrollIntoView = jest.fn();
     renderWithContext(<MessengerPage />);
-    const initialLabel = getInitialPageLabel();
+    const initialLabel = getInitialTitleLabel();
+    const commandsLabel = getCommandsLabel();
 
-    console.log(initialLabel.innerHTML);
-    expect(initialLabel.innerHTML).toBe(`${messages.title} ${defaultName}`);
+    expect(initialLabel.innerHTML).toBe(`${messages.title} ${defaultNick}`);
+    expect(commandsLabel.innerHTML).toBe(`${messages.commands}`);
+  });
+
+  it("render chatboxes after setting in store online users", () => {
+    const { store } = renderWithContext(<MessengerPage />);
+
+    act(() => {
+      store.dispatch(setOnlineUsers(DUMMY_ONLINE_USERS));
+    });
+
+    const chatboxInputs = getChatboxesNewMessageInputs();
+    expect(chatboxInputs.length).toBe(DUMMY_ONLINE_USERS.length);
   });
 });
 
-const getInitialPageLabel = () => {
-  return screen.getByRole("heading", {
-    name: /hello stranger/i,
+const getInitialTitleLabel = () => {
+  return screen.getByText(`${messages.title} ${defaultNick}`, { exact: false });
+};
+
+const getCommandsLabel = () => {
+  return screen.getByText(`${messages.commands}`, { exact: false });
+};
+
+const getChatboxesNewMessageInputs = () => {
+  return screen.getAllByPlaceholderText(`${messages.newMessagePlaceholder}`, {
+    exact: false,
   });
 };
