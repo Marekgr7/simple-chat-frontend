dcd14e53580f831d9923940cbc4161eda5611808
Marekgr7 <Marekgr7@gmail.com>
2022-04-26 11:39:37 +0200

    dummy data moved to separed file

--- src/MessengerPage/messengerSlice.test.js
+++ src/MessengerPage/messengerSlice.test.js
@@ -7,58 +7,7 @@ import messengerReducer, {
   setSpecificMessageAdditionalStyles,
   setCountdownDetails,
 } from "./messengerSlice";
-import { messagesTypes } from "../realtimeCommunication/chat/chatController";
-
-const DUMMY_ONLINE_USERS = [
-  {
-    socketId: "123",
-    nick: "TestUser",
-  },
-  {
-    socketId: "345",
-    nick: "Test",
-  },
-  {
-    socketId: "567",
-    nick: null,
-  },
-];
-
-const DUMMY_CHAT_HISTORY = [
-  {
-    socketId: "123",
-    messages: [
-      {
-        id: 1,
-        type: messagesTypes.CREATED,
-        content: "Hello world",
-        additionalStyles: {
-          fontSize: "16px",
-          color: "grey",
-        },
-      },
-      {
-        id: 2,
-        type: messagesTypes.RECEIVED,
-        content: "Hello",
-        additionalStyles: {},
-      },
-      {
-        id: 3,
-        type: messagesTypes.WARNING,
-        content: "Command invalid. Please enter valid command",
-        additionalStyles: {},
-      },
-      {
-        id: 4,
-        type: messagesTypes.CREATED,
-        content:
-          "Command invalid. Please enter valid command. Command invalid. Please enter valid command. Command invalid. Please enter valid command.",
-        additionalStyles: {},
-      },
-    ],
-  },
-];
+import { DUMMY_CHAT_HISTORY, DUMMY_ONLINE_USERS } from "./messengerSlice.dummy";
 
 const myNick = "Tester";
 
